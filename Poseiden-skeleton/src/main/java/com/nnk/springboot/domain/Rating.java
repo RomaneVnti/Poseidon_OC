b898fa21e8d337c5989d85fb865baa3883a4c589
romane <romane.valenti@gmail.com>
2025-05-16 18:31:53 +0200

    Initial commit: récupération du projet pour customisation

---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -0,0 +1,34 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+@Table(name = "rating")
+public class Rating {
+    // TODO: Map columns in data table RATING with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+
+    @NotNull
+    @NotBlank
+    String moodysRating;
+
+    @NotNull
+    @NotBlank
+    String sandRating;
+
+    @NotNull
+    @NotBlank
+    String fitchRating;
+
+    @NotNull
+    Integer orderNumber;
+}
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -0,0 +1,51 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+/**
+ * Model de l'entité Trade
+ */
+@Entity
+@Data
+@Table(name = "trade")
+public class Trade {
+    // TODO: Map columns in data table TRADE with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer tradeId;
+
+    @NotNull
+    @NotBlank
+    String account;
+
+    @NotNull
+    @NotBlank
+    String type;
+
+    @NotNull
+    Double buyQuantity;
+
+    Double sellQuantity;
+    Double buyPrice;
+    Double sellPrice;
+    String benchmark;
+    Timestamp tradeDate;
+    String security;
+    String status;
+    String trader;
+    String book;
+    String creationName;
+    Timestamp creationDate;
+    String revisionName;
+    Timestamp revisionDate;
+    String dealName;
+    String dealType;
+    String sourceListId;
+    String side;
+}
